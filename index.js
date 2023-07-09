60e9bbc068d19e2e009ab420d8e21448708b39f8
dacc77 <123707776+dacc77@users.noreply.github.com>
2023-07-09 15:13:11 -0700

    edited pathway of npc

--- index.js
+++ index.js
@@ -5,7 +5,35 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+
+//north 1400 .
+//east 1200.
+//south 300.
+//east 1500.
+//south 1500.
+//west 2700
+//north 400
+
+npc.walkEast(500, () => {
+     npc.walkNorth(1400, () => {
+        npc.walkEast(1200, () => {
+             npc.walkSouth(300, () => {
+                 npc.walkEast(1500, ()=> {
+                    npc.walkSouth(1500, ()=> {
+                        npc.walkWest(2700, ()=> {
+                            npc.walkNorth(400)
+
+                        })
+                    })
+
+                 })
+
+             })
+        })
+    })
+})
+
+
 
 // Create the inventory
 const inventory = newInventory()
